52f7398b0ea9117b0781ca7d6b02789a31acc608
Ihab Awadallah <ihab@example.com>
2025-06-15 13:57:10 -0400

    Initial deploy ready project

---
+++ autohawk_site/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>AutoHawk</h1>
+      <p>America’s easiest used car marketplace</p>
+    </div>
+  );
+}
